45d1c34e1fca940755223ce32c186b331082c795
Breno Ramalho Lemes <breno@br-lemes.net>
2022-07-31 14:20:46 -0400

    Refactor --exercise=assembly-line --track=rust

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,13 +3,15 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    match speed {
-        0 => 0.0,
-        1..=4 => speed as f64 * 221.0,
-        5..=8 => speed as f64 * 221.0 * 0.9,
-        9 | 10 => speed as f64 * 221.0 * 0.77,
-        _ => unreachable!(),
-    }
+    speed as f64
+        * 221.0
+        * match speed {
+            0 => 0.0,
+            1..=4 => 1.0,
+            5..=8 => 0.9,
+            9 | 10 => 0.77,
+            _ => unreachable!(),
+        }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
