0a7f826eeb09e32a25620d0277a2b07433261c08
Breno Ramalho Lemes <breno@br-lemes.net>
2024-01-18 00:28:30 -0400

    Approach --track=php --exercise=leap

--- php/leap/Leap.php
+++ php/leap/Leap.php
@@ -4,5 +4,14 @@ declare(strict_types=1);
 
 function isLeap(int $year): bool
 {
-    return $year % 100 === 0 ? $year % 400 === 0 : $year % 4 === 0;
+    if ($year % 4 !== 0) {
+        return false;
+    }
+    if ($year % 100 !== 0) {
+        return true;
+    }
+    if ($year % 400 !== 0) {
+        return false;
+    }
+    return true;
 }
