1f96ed940a1348c7e0ffe319a8ee4908c18377b0
Breno Ramalho Lemes <breno@br-lemes.net>
2024-05-11 20:36:11 -0400

    Really solve --track=zig --exercise=all-your-base

--- zig/all-your-base/all_your_base.zig
+++ zig/all-your-base/all_your_base.zig
@@ -19,10 +19,23 @@ pub fn convert(
     if (output_base < 2) return {
         return ConversionError.InvalidOutputBase;
     };
-    for (digits) |digit| {
-        if (input_base >= digit) {
+
+    var base_number: u32 = 0;
+    for (digits, 0..) |d, i| {
+        if (d < 0 or d >= input_base) {
             return ConversionError.InvalidDigit;
         }
+        base_number += d * std.math.pow(u32, input_base, @as(u32, @truncate(digits.len - 1 - i)));
     }
-    return allocator.alloc(u32, 0);
+
+    const output_length = if (base_number <= 1) 1 else std.math.log(u32, output_base, base_number) + 1;
+    var output_digits = try allocator.alloc(u32, output_length);
+    errdefer allocator.free(output_digits);
+
+    for (0..output_length) |i| {
+        output_digits[output_length - 1 - i] = @rem(base_number, output_base);
+        base_number = @divTrunc(base_number, output_base);
+    }
+
+    return output_digits;
 }
