545b92d6bcf2e07a3a0fdb23e8cb5ce8248c8445
Breno Ramalho Lemes <breno@br-lemes.net>
2025-08-11 11:26:41 -0400

    chore: removido código ruby duplicado

--- ruby/hello-world/hello_world_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'hello_world'
-
-class HelloWorldTest < Minitest::Test
-    def test_say_hi
-        assert_equal "Hello, World!", HelloWorld.hello
-    end
-end
