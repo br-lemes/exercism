1d70fad767a37444c40cd5f535012c526188cf0e
Breno Ramalho Lemes <breno@br-lemes.net>
2025-08-11 11:05:17 -0400

    chore: removido código python duplicado

--- python/leap/leap.py
+++
@@ -1,9 +0,0 @@
-"""Leap"""
-
-def leap_year(year):
-    """Return True if year is a leap year.
-
-    :param year: int
-    :return: bool
-    """
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
